1d846646b6c8b8d369bbc4f72d2a34d5244fb748
Schaeran <schaeran1994@hotmail.com>
2021-11-09 00:32:33 +0100

    Create ENUM for the Twitter API

---
+++ Twitter/Enum_Twitter.py
@@ -0,0 +1,14 @@
+import enum
+
+class API_Version(enum.Enum):
+    OLD =  'https://api.twitter.com/1.1/',
+    CURRENT = 'https://api.twitter.com/2/'
+
+class API_Endpoint(enum.Enum):
+    TWEET = 'tweets/{id}',
+    TWEETS = 'users/{id}/tweets',
+    FOLLOWER = 'users/{id}/followers',
+    FOLLOWING = 'users/{id}/following',
+    MENTIONS = 'users/{id}/mentions',
+    LIKED_BY = 'tweets/{id}/liking_users',
+    RETWEETED_BY = 'tweets/{id}/retweeted_by'
